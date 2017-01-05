1228b46dde09ff188f6867f48515246c6d4c09d1
Lauren <lauren.weston@student.manchester.ac.uk>
2017-01-05 22:10:39 +0000

    My actual code in my first proper commit

---
+++ cSparks.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Phrase
+{
+	string phrase;
+	string[] followOn;
+
+	public Phrase(string phrase, string[] followOn) {
+		this.phrase = phrase;
+		this.followOn = followOn;
+	}
+}
+
+class cSparks
+{
+	static void Main() {
+		Console.WriteLine("Wahey");
+	}
+}
