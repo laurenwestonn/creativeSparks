6b098b46c0b7c84495c12b70d69c50488f34de99
Lauren <lauren.weston@student.manchester.ac.uk>
2017-02-26 21:44:48 +0000

    Load line into lines list. Todo: Write accessors to get eg phrase at ID x

--- CSparks/ClassLibrary1/Parser.cs
+++ CSparks/ClassLibrary1/Parser.cs
@@ -6,52 +6,81 @@ using System.Threading.Tasks;
 
 namespace Engine
 {
-    public static class Parser
-    {
-    	struct Line {
-    		public int ID;
-    		public int nextID;
-    		public string phrase;
-    		public int rating;
-    		public List<string> attracts;
-    		public List<string> offends;
-    		
-    		public Line(int I, int nI, string p, int r, List<string> a, List<string> o)
-    		{
-    			ID = I;
-    			nextID = nI;
-    			phrase = p;
-    			rating = r;
-    			attracts = a;
-    			offends = o;
-    		}
-    	}
-    	
-    	void Start()
-    	{
-    		string file = "phrases.txt";
-    		
-    		lines = new List<Line>();
-    		LoadDialogue(file); 
-    	}
-    
-    	public LoadDialogue(string filename)
-    	{
-    		string path = "Assets/Resources/" + filename;
-    		string lineOfText;
-    		StreamReader r = new StreamReader(filename);
-    		
-    		using(r)
-    		{
-    			do {
-    				line = r.ReadLine();
-    				if(line != null) {
-    					
-    				}
-    				
-    			}
-    		}
-    		
-    	}
- 		}   
+  public static class Parser
+  {
+  	struct Line {
+  		public int ID;
+  		public int nextID;
+  		public string phrase;
+  		public int rating;
+  		public List<string> attracts;
+  		public List<string> offends;
+  		
+  		public Line(int I, int nI, string p, int r, List<string> a, List<string> o)
+  		{
+  			ID = I;
+  			nextID = nI;
+  			phrase = p;
+  			rating = r;
+  			attracts = a;
+  			offends = o;
+  		}
+  	}
+  	
+  	void Start()
+  	{
+  		string file = "phrases.txt";
+  		
+  		lines = new List<Line>();
+  		LoadDialogue(file); 
+  	}
+  	
+  	void OnUpdate()
+  	{
+  	
+  	}
+  
+  	public LoadDialogue(string filename)
+  	{
+  		string path = "Assets/Resources/" + filename;
+  		string lineOfText;
+  		string[] arrLine;
+  		Line line;
+  		StreamReader r = new StreamReader(filename);
+  		
+  		using(r) {
+  			do {
+  				lineOfText = r.ReadLine();
+  				
+  				if(lineOfText != null) {
+  					arrLine = lineOfText.Split(',');
+  					
+						List<string> attracts;
+						List<string> offends;
+						
+  					//populate the attracts list
+  					foreach(string a in arrLine[4].Split('.')) {
+  						attracts.Add(a);
+  					}
+  					
+						//populate the offends list
+  					foreach(string o in arrLine[5].Split('.')) {
+  						offends.Add(o);
+  					}
+  					line = new line(Int32.Parse(arrLine[0]),	//ID
+  													Int32.Parse(arrLine[1]),	//Next ID
+  													arrLine[2],								//Phrase
+  													Int32.Parse(arrLine[3]),	//Rating
+  													attracts,									//Attracts
+  													offends										//Offends
+  													)
+  					lines.Add(line);
+  				}
+  				
+  			}
+  			while(line != null);
+  			r.Close();
+  		}
+  	}
+	}
 }
